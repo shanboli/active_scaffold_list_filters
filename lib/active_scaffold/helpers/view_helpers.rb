87738ae817ad7b7229a7050737e31df9c93c86e5
Enrico Bianco <enricob@gmail.com>
2010-02-02 09:54:25 -0500

    Instead of chaining on active_scaffold_includes, chain on active_scaffold_stylesheets and active_scaffold_ie_stylesheets. This way, the stylesheets should get properly included when referenced separately as well as when called on by active_scaffold_includes

--- lib/active_scaffold/helpers/view_helpers.rb
+++ lib/active_scaffold/helpers/view_helpers.rb
@@ -1,12 +1,15 @@
 module ActiveScaffold
   module Helpers::ViewHelpers
-
-  # Add the export plugin includes
-  def active_scaffold_includes_with_list_filter(frontend = :default)
-    css = stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path('list_filter-stylesheet.css', frontend))
-    ie_css = stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path("list_filter-stylesheet-ie.css", frontend))
-    active_scaffold_includes_without_list_filter + "\n" + css + "\n<!--[if IE]>" + ie_css + "<![endif]-->\n"
-  end
-  alias_method_chain :active_scaffold_includes, :list_filter
+  
+    def active_scaffold_stylesheets_with_list_filter(frontend = :default)
+      active_scaffold_stylesheets_without_list_filter + [ActiveScaffold::Config::Core.asset_path('list_filter-stylesheet.css', frontend)]
+    end
+    alias_method_chain :active_scaffold_stylesheets, :list_filter
+    
+    def active_scaffold_ie_stylesheets_with_list_filter(frontend = :default)
+      active_scaffold_ie_stylesheets_without_list_filter + [ActiveScaffold::Config::Core.asset_path('list_filter-stylesheet-ie.css', frontend)]
+    end
+    alias_method_chain :active_scaffold_ie_stylesheets, :list_filter
+  
   end
 end
